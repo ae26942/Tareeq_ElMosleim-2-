1d9f8376286f0b41b25875c60a535a82d3ed6c22
ae26942 <pzkt@outlook.com>
2025-09-14 00:50:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// تسجيل Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('Service Worker registered', reg))
+        .catch(err => console.log('Service Worker registration failed', err));
+    });
+}
+
 // قائمة ببيانات الأقسام مع الألوان والأيقونات
 const sections = [
     {
