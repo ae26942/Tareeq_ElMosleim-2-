0c6ec8bda00ff7b256f07fd9d4af3715fd897615
ae26942 <pzkt@outlook.com>
2025-09-14 02:26:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,39 @@ const urlsToCache = [
     '/',
     'index.html',
     'style.css',
-    'app.js',
+    'app.const CACHE_NAME = 'muslim-way-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './assets/icon-192.png',
+    './assets/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+',
     'manifest.json'
 ];
 
